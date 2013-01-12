09483c0874b11dedd1259104979d3ce9ac9288d2
Lorefnon <lorefnon@gmail.com>
2013-01-13 02:53:08 +0530

    Generate and configure devise models and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Necromancer::Application.routes.draw do
+  devise_for :users
+
+  root :to => 'home#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
