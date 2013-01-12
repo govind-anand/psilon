2637572b3b40e9ba035e257a3e8f90ec7c0e7c89
Lorefnon <lorefnon@gmail.com>
2013-01-13 01:55:00 +0530

    Fresh new Rails application skeleton
    Command: rails new necromancer --skip-javascrpt --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Necromancer::Application.config.secret_token = '675150d80dea729091920b55872d907a2d494591375fc6085d6b7bfdd72a4ed0f6c8d48a1c4e532116eb347aebbd8d513c09d46a4fa894434492112ba73b0edd'
